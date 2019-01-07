47c245341c496f5a5ab4e37efce702e8ef309203
wxh_leo <754557634@qq.com>
2019-01-07 23:25:42 +0800

    添加发布笔记接口

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,6 @@
 // routes/notes.js
+const Joi = require('joi');
+const fs = require('fs');
 const GROUP_NAME = 'notes';
 const models = require('../node_modules/.bin/models');
 const { paginationDefine } = require('../utils/router-helper');
@@ -37,13 +39,47 @@ module.exports = [
       let userId = request.auth.credentials.userId;
       let result = {
         success: false,
-        data: {
-          title: '',
-          content: '',
-          thumb_url: '',
-          labels: ''
-        }
+        statu: 0
       }
+      let path = 'public/resources/cover/'+ Date.now() +'.jpg';
+      let base64 = thumb_url.replace(/^data:image\/\w+;base64,/, "");
+      let dataBuffer = new Buffer(base64, 'base64');
+      console.log('dataBuffer是否是Buffer对象：'+Buffer.isBuffer(dataBuffer));
+      fs.writeFile(path,dataBuffer,function(err){
+        if(err){
+            console.log(err);
+        }else{
+           console.log('写入成功！');
+        }
+      })
+      await models.notes.upsert({
+        title: title,
+        content: content,
+        thumb_url: path,
+        labels: labels,
+        auth_id: userId
+      }).then((note) => {
+        result.success = true;
+        result.statu = 1;
+        reply(result);
+        console.log(note)
+      }).catch((err) => {
+        reply(result);
+        console.log('笔记插入失败')
+        console.log(err);
+      })
+    },
+    config: {
+      validate :{
+        payload: {
+          title: Joi.string().required().description('笔记标题'),
+          content: Joi.string().required().description('笔记内容'), 
+          thumb_url: Joi.string().required().description('笔记封面'),
+          labels: Joi.string().required().description('标签')
+        }
+      },
+      tags: ['api', GROUP_NAME],
+      description: '创建笔记',
     }
   }
 ]
