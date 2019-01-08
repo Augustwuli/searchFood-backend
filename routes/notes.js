97392543aae0bbf0e443ae146122ea24beefad7b
wxh_leo <754557634@qq.com>
2019-01-08 20:29:55 +0800

    添加多图片上传和获取笔记列表路由

--- routes/notes.js
+++ routes/notes.js
@@ -8,39 +8,67 @@ const { paginationDefine } = require('../utils/router-helper');
 module.exports = [
   {
     method: 'GET',
-    path: `/${GROUP_NAME}`,
+    path: `/${GROUP_NAME}/list`,
     handler: async (request, reply) => {
-      const { rows: results, count: totalCount } = await models.notes.findAndCountAll({
-        attributes: [
-            'id', 'auth_id', 'title'
-        ],
+      let result = {
+        success: false,
+        data: {
+          notes: [],
+          count: 0
+        },
+        statu: 0
+      };
+      await models.notes.findAndCountAll({
+        attributes: ['id', 'auth_id', 'title', 'thumb_url', 'read_num', 'star_num', 'comment_num'],
         limit: request.query.limit,
         offset: (request.query.page - 1) * request.query.limit,
-    });
-    // 开启分页插件，返回的数据结构里，需要带上 result 与 totalCount 两个字段
-    reply({ results, totalCount });
+      }).then((notes) => {
+        result.success = true;
+        result.data.notes = notes.rows;
+        result.data.count = notes.count;
+      }).catch((err) => {
+        console.error('标签查询失败');
+        console.log(err)
+      })
+      reply(result)
     },
     config: {
-        validate :{
-            query: {
-                ...paginationDefine
-            }
-        },
-        auth: false,
-        tags: ['api', GROUP_NAME],
-        description: '获取笔记列表',
+      validate :{
+        query: {
+          ...paginationDefine
+        }
+      },
+      tags: ['api', GROUP_NAME],
+      description: '用于测试的笔记列表接口',
+      auth: false,
     }
   },
   {
     method: 'POST',
     path: `/${GROUP_NAME}/publish`,
     handler: async (request, reply) => {
-      const { title, content, thumb_url, labels } = request.payload;
+      const { title, content, thumb_url, labels, imgs } = request.payload;
       let userId = request.auth.credentials.userId;
       let result = {
         success: false,
         statu: 0
       }
+      let images = imgs.split('-');
+      let paths = [];
+      for (let i = 0; i < images.length; i++) {
+        paths[i] = 'public/resources/img/'+ Date.now() +'.jpg';
+        let base64 = images[i].replace(/^data:image\/\w+;base64,/, "");
+        let dataBuffer = new Buffer(base64, 'base64');
+        console.log('dataBuffer是否是Buffer对象：'+Buffer.isBuffer(dataBuffer));
+        fs.writeFile(paths[i],dataBuffer,function(err){
+          if(err){
+              console.log(err);
+          }else{
+            console.log(`${i}张写入成功！`);
+          }
+        })
+      }
+      let img = paths.join(',');
       let path = 'public/resources/cover/'+ Date.now() +'.jpg';
       let base64 = thumb_url.replace(/^data:image\/\w+;base64,/, "");
       let dataBuffer = new Buffer(base64, 'base64');
@@ -57,7 +85,8 @@ module.exports = [
         content: content,
         thumb_url: path,
         labels: labels,
-        auth_id: userId
+        auth_id: userId,
+        imgs: img
       }).then((note) => {
         result.success = true;
         result.statu = 1;
@@ -75,7 +104,8 @@ module.exports = [
           title: Joi.string().required().description('笔记标题'),
           content: Joi.string().required().description('笔记内容'), 
           thumb_url: Joi.string().required().description('笔记封面'),
-          labels: Joi.string().required().description('标签')
+          labels: Joi.string().required().description('标签'),
+          imgs: Joi.string().required().description('图片'),
         }
       },
       tags: ['api', GROUP_NAME],
--- routes/statics.js
+++ routes/statics.js
@@ -21,5 +21,43 @@ module.exports = [
       description: '返回头像',
       auth: false
     },
+  },
+  {
+    method: 'GET',
+    path: '/public/resources/img/{path}',
+    handler: {
+      file: function (request) {
+        return Path.join(__dirname, `../public/resources/img/${request.params.path}`);
+      }
+    },
+    config: {
+      validate: {
+        params: {
+          path: Joi.string().required(),
+        }
+      },
+      tags: ['api', GROUP_NAME],
+      description: '返回图片',
+      auth: false
+    },
+  },
+  {
+    method: 'GET',
+    path: '/public/resources/cover/{path}',
+    handler: {
+      file: function (request) {
+        return Path.join(__dirname, `../public/resources/cover/${request.params.path}`);
+      }
+    },
+    config: {
+      validate: {
+        params: {
+          path: Joi.string().required(),
+        }
+      },
+      tags: ['api', GROUP_NAME],
+      description: '返回封面图片',
+      auth: false
+    },
   }
 ];
