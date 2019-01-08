13251dfc208bb4ff3627183c6d2477681f06429d
wxh_leo <754557634@qq.com>
2019-01-08 22:07:40 +0800

    添加获取笔记内容的路由

--- routes/notes.js
+++ routes/notes.js
@@ -111,5 +111,41 @@ module.exports = [
       tags: ['api', GROUP_NAME],
       description: '创建笔记',
     }
+  },
+  {
+    method: 'GET',
+    path: `/${GROUP_NAME}/{noteId}`,
+    handler: async (request, reply) => {
+      let result = {
+        success: false,
+        data: {},
+        statu: 0
+      };
+      const { noteId } = request.params;
+      await models.notes.findOne({
+        where: {
+          'id': noteId
+        }
+      }).then((note) => {
+        result.success = true;
+        result.data.id = note.get('id');
+        result.data.title = note.get('title');
+        result.data.labels = note.get('labels');
+      }).catch((err) => {
+        console.log('笔记内容查找失败');
+        console.log(err);
+      })
+      reply(result);
+    },
+    config: {
+      validate :{
+        params: {
+          noteId: Joi.string().required(),
+        }
+      },
+      tags: ['api', GROUP_NAME],
+      description: '查找笔记内容',
+      auth: false
+    }
   }
 ]
